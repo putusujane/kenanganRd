7ab4f99dc425b5fe15392cfae068291231f35d4c
putusujane <blogibagi@gmail.com>
2023-01-20 12:56:50 +0700

    Menambah fitur efek ripple, responsive, dll

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // Feather Replace
 feather.replace();
+
+// Menu Hamburger
+
+const hamburgerOpen = () => {
+    document.getElementById("hamburgerOpen").style.display = "none";
+    document.getElementById("hamburgerClose").style.display = "block";
+    document.getElementById("mobile-nav").style.display = "block";
+}
+
+const hamburgerClose = () => {
+    document.getElementById("hamburgerOpen").style.display = "block";
+    document.getElementById("hamburgerClose").style.display = "none";
+    document.getElementById("mobile-nav").style.display = "none";
+}
