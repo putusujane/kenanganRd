ac022f1cfebefc159ace82039f60d4d5cce2e804
putusujane <blogibagi@gmail.com>
2023-01-20 17:20:36 +0700

    Add fitur textarea bagian contact

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@
 feather.replace();
 
 // Menu Hamburger
-
 const hamburgerOpen = () => {
     document.getElementById("hamburgerOpen").style.display = "none";
     document.getElementById("hamburgerClose").style.display = "block";
